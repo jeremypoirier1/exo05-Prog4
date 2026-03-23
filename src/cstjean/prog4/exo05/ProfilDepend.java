d5835c1e747d045f9bdb0d701b672d1c56eed966
jay <6302610@cstjean.qc.ca>
2026-03-23 10:42:04 -0400

    profil depend

---
+++ src/cstjean/prog4/exo05/ProfilDepend.java
@@ -0,0 +1,39 @@
+﻿package cstjean.prog4.exo05;
+
+public class ProfilDepend extends Portefeuille {
+    public ProfilDepend(double objectif, ActifsIterator actifs, String name) {
+        super(objectif, actifs, name, "Ça dépend");
+    }
+
+    @Override
+    public boolean verifyObjectif() {
+        if (isActionDown()) {
+            if ()
+            System.out.println("Action a la baisse");
+            sellAction();
+            return true;
+        }
+        System.out.println("Rien effectué");
+        return false;
+    }
+
+    public boolean isActionDown() {
+        for (ActifsIterator it = actifs; it.hasNext(); ) {
+            Actif actif = it.next();
+            if (actif.getAction().getIsDown()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isActionDownBy5 {
+        for (ActifsIterator it = actifs; it.hasNext(); ) {
+            Actif actif = it.next();
+            if (actif.getAction().getAncien() - actif.getAction().getPrix() >= 5) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
