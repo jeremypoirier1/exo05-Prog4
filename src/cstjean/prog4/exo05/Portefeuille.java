48464932e27dc63b88ecc18766eff8f68c4f19fb
jay <6302610@cstjean.qc.ca>
2026-03-23 09:59:40 -0400

    Portefeuille

--- src/cstjean/prog4/exo05/Portefeuille.java
+++ src/cstjean/prog4/exo05/Portefeuille.java
@@ -1,38 +1,53 @@
 ﻿package cstjean.prog4.exo05;
 
-import java.util.List;
-
 public abstract class Portefeuille {
     private final double objectif;
-    private final List<Action> actifs;
+    private final ActifsIterator actifs;
     private final double valeurInit;
     private final String name;
+    private final String profil;
 
-    public Portefeuille(double objectif,  actifs, String name) {
+    public Portefeuille(double objectif, ActifsIterator actifs, String name, String profil) {
         this.objectif = objectif;
         this.actifs = actifs;
         this.name = name;
+        this.profil = profil;
         valeurInit = getValeur();
     }
 
     public final void verificationPortefeuille() {
-
+        System.out.println("###############");
+        System.out.printf("Évaluation du portefeuille «%s» avec profil «%s»\n", name, profil);
+        System.out.println("Résultat :");
+        if (verifyObjectif()) {
+            sellAction();
+        }
+        System.out.println("###############");
     }
 
-    public void verifyObjectif() {
-        if (objectif >= actifs.getValeur()) {
+    public boolean verifyObjectif() {
+        if (objectif >= getValeur()) {
             System.out.println("Objectif atteint");
             sellAction();
-            return;
+            return true;
         }
         System.out.println("Objectif non atteint");
+        return false;
     }
 
     public void sellAction() {
         System.out.printf("Vente d'action du portefeuille «%s»\n", name);
         System.out.println("Valeur initiale : " + valeurInit);
-        double valeurActuel = actifs.getValeur();
+        double valeurActuel = getValeur();
         System.out.println("Valeur actuel : " + valeurActuel);
         System.out.println("Profit : " + (valeurActuel - valeurInit));
+    }
+
+    public double getValeur() {
+        double value = 0.0;
+        for (Actif actif : actifs) {
+            value += actif.getAction().getPrix() * actif.getQuantite();
+        }
+        return value;
     }
 }
